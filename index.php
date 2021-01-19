92d51828cfd46b3cc909184e76cedef49c850bdd
AdrianaMargiotta <adrianamargiotta@protonmail.com>
2021-01-19 15:43:21 +0100

    employees without Exceptions

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dipedenti</title>
+    <title>Employees without Exceptions</title>
 </head>
 <body>
     <?php
